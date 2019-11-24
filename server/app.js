44dafff826b63056071c0ce6859ff6f20ccbbbeb
Johnathon Hutt <johnathonhutt@gmail.com>
2019-11-23 19:03:14 -0500

    build get all, post new entry and delete all routes

--- server/app.js
+++ server/app.js
@@ -9,21 +9,21 @@ const mongoose = require ("mongoose");
 //set new instance of express
 const app = express();
 
+
 //use body-parser
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+
 //connect to mongoDB -- refLinksDB
-mongoose.connect("mongodb://localhost:27017/refLinksDB", {
-  useNewUrlParser: true
-});
+mongoose.connect("mongodb://localhost:27017/refLinksDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 //define mongoose Schema
 const refLinkSchema = {
-  name: String,
-  clicNum: Number
+  name: {type: String, required: true},
+  clickNum: {type: Number, default: 0}
 };
 
 const RefLink = mongoose.model("RefLink", refLinkSchema);
@@ -38,6 +38,7 @@ app.get("/", function(req, res){
 
 //route for all refLinks
 app.route("/reflinks")
+  //Read route - gets all db entries
   .get(function(req, res) {
     RefLink.find(function(err, foundRefLinks) {
       if (!err) {
@@ -46,7 +47,45 @@ app.route("/reflinks")
         res.send(err);
       }
     });
+  })
+  //Create route - add new db entry
+  .post(function(req, res) {
+    console.log(req.body.name);
+    console.log(req.body.clickNum);
+
+    const newRefLink = new RefLink({
+      name: req.body.name,
+      clickNum: req.body.clickNum,
+    });
+
+    console.log(newRefLink);
+    newRefLink.save(function(err) {
+      if (!err) {
+        res.send(`New refLink: ${req.body.name}, was successfully added`);
+      } else {
+        res.send(`There was an error adding ${req.body.name} entry: ${err}`);
+      }
+    });
+  })
+  //delete all entries
+  .delete(function(req, res) {
+    RefLink.deleteMany(function(err) {
+      if (!err) {
+        res.send("Successfully deleted all articles.");
+      } else {
+        res.send(err);
+      }
+    });
   });
+
+
+//routes for specific entries
+
+
+
+
+
+
 
 
 
