5f94b8b7b398fe4391f4621544bf0b3f53cc10d3
Johnathon Hutt <johnathonhutt@gmail.com>
2019-11-25 11:10:31 -0500

    update server

--- server/app.js
+++ server/app.js
@@ -16,25 +16,25 @@ app.use(bodyParser.urlencoded({
 }));
 
 
+const port = process.env.PORT || 5000;
+
+
+//Set uri string to mLab (a heroku add-on database) MongoDB database
+const uristring = process.env.MONGODB_URI || "mongodb://localhost:27017/refLinksDB";
+
+
 //connect to mongoDB -- refLinksDB
-mongoose.connect("mongodb://localhost:27017/refLinksDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uristring, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 //define mongoose Schema
 const refLinkSchema = {
-  name: {type: String, required: true},
+  name: {type: String, required: true, unique: true},
   clickNum: {type: Number, default: 0}
 };
 
 const RefLink = mongoose.model("RefLink", refLinkSchema);
 
-
-//setup routes
-
-//test home/get route
-app.get("/", function(req, res){
-  res.send("It worked, great job!!");
-});
 
 //route for all refLinks
 app.route("/reflinks")
@@ -52,13 +52,10 @@ app.route("/reflinks")
   .post(function(req, res) {
     console.log(req.body.name);
     console.log(req.body.clickNum);
-
     const newRefLink = new RefLink({
       name: req.body.name,
       clickNum: req.body.clickNum,
     });
-
-    console.log(newRefLink);
     newRefLink.save(function(err) {
       if (!err) {
         res.send(`New refLink: ${req.body.name}, was successfully added`);
@@ -71,7 +68,7 @@ app.route("/reflinks")
   .delete(function(req, res) {
     RefLink.deleteMany(function(err) {
       if (!err) {
-        res.send("Successfully deleted all articles.");
+        res.send("Successfully deleted all entries.");
       } else {
         res.send(err);
       }
@@ -80,15 +77,50 @@ app.route("/reflinks")
 
 
 //routes for specific entries
+  app.route("/reflinks/:refLinkName")
+    //Find link by name - shouldn't be needed for app but just in case...
+    .get(function(req,res){
+      console.log(req.params.refLinkName);
+      RefLink.findOne({name: req.params.refLinkName}, function(err, entryFound){
+        if (entryFound) {
+          res.send(entryFound);
+        } else {
+          res.send("No matching Link");
+        }
+      });
+    })
+    //Find link by name - update with provided information
+    .patch(function(req, res){
+      RefLink.update(
+        //conditions
+        {name: req.params.refLinkName},
+        //updates
+        {$set: req.body},
+        function(err, result){
+          if (!err) {
+            res.send("Link entry was patched successfully.");
+          } else {
+            res.send(err);
+          }
+        }
+      );
+    })
+    //Find link by name - delete specific entry
+    .delete(function(req, res){
+      RefLink.deleteOne(
+        //conditions
+        {title: req.params.refLinkName},
+        function(err){
+          if (!err) {
+            res.send("Successfully deleted entry.");
+          } else {
+            res.send(err);
+          }
+        }
+      );
+    });
 
 
-
-
-
-
-
-
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Server is running on port 3000");
+app.listen(port, function(){
+  console.log(`Server is running on port ${port}`);
 });
